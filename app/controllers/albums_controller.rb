1a4ac45640666d1b55e828fe325beb760549784f
Jennifer Georgevich <jenngeorge@berkeley.edu>
2016-08-04 15:38:48 -0700

    create new album for band

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,16 +1,55 @@
 class AlbumsController < ApplicationController
+
+  # def index
+  #   @albums = albums.all
+  #   render :index
+  # end
+
   def new
+    @album = Album.new
+    @bands = Band.all
+    @current_band = Band.find_by_id(params[:band_id])
+    render :new
   end
 
   def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @album = Album.find(params[:id])
   end
 
   def update
+    @album = Album.find(params[:id])
+    if @album.update_attributes(album_params)
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :edit
+    end
+  end
+
+
+  def show
+    @album = Album.find_by_id(params[:id])
   end
 
   def destroy
+    @album = Album.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to albums_url
   end
 
-  def band_params
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :band_id)
   end
 end
