4dd56b01f0fb05e1a5bdeb6ae2322d1894497cf6
Jennifer Georgevich <jenngeorge@berkeley.edu>
2016-08-04 23:49:34 -0700

    add stylish track lyrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    each_line = lyrics.lines
+    with_notes = each_line.map {|line| line = "&#9835; #{h(line)}"}.join
+    "<pre>#{with_notes}</pre>".html_safe
+  end
 end
